affdfba85bc14b812396bbb5bd37ec4b8d5004a4
고해석 <84788785+wfos3241@users.noreply.github.com>
2025-04-24 10:06:24 +0900

    테스트 2

--- Git_Java/src/git_java/Git_javac.java
+++ Git_Java/src/git_java/Git_javac.java
@@ -1,16 +1,20 @@
 package git_java;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Git_javac {
 
 	public static void main(String[] args) {
 		
+		Random rd = new Random();
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		
 		System.out.println(num);
-
+		
+		
+		
 	}
 
 }
