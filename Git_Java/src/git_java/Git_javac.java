d043f0b9a002395f3002dbe3a46fa9f6d18eee6e
고해석 <84788785+wfos3241@users.noreply.github.com>
2025-04-24 09:34:54 +0900

    테스트용 커밋

---
+++ Git_Java/src/git_java/Git_javac.java
@@ -0,0 +1,16 @@
+package git_java;
+
+import java.util.Scanner;
+
+public class Git_javac {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		System.out.println(num);
+
+	}
+
+}
